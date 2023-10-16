7216eeab2b10ca940a32b32104a549323b8a9fc0
anniepauline <anniepauline.23@gmail.com>
2023-10-16 21:52:50 +0530

    dark theme

--- vite.config.ts
+++ vite.config.ts
@@ -19,16 +19,15 @@ limitations under the License.
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-
 import monacoEditorPlugin, {
   IMonacoEditorOpts,
 } from "vite-plugin-monaco-editor";
 
 // https://vitejs.dev/config/
 
-const optionss: IMonacoEditorOpts = {
-  customWorkers: [{label:"yaml",entry:"monaco-yaml/yaml.worker.js"}],
-  customWorkers: [{label:"yaml",entry:"monaco-yaml/yaml.worker.js"}"editorWorkerService"]
+const options: IMonacoEditorOpts = {
+  customWorkers: [{ label: "yaml", entry: "monaco-yaml/yaml.worker.js" }],
+  languageWorkers: ["editorWorkerService"]
 };
 export default defineConfig({
   plugins: [monacoEditorPlugin.default(options), react()],
